06bf10f815ebd40643e7f7bc51c0d3146af9cc23
Barun Ghosh <barungh@hotmail.com>
2022-05-17 09:15:39 +0530

    Add custom directory

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,16 @@
+-- Hare Krishna
+local M = {}
+
+M.ui = {
+   theme = "gruvchad",
+}
+
+M.plugins = {
+   options = {
+      lspconfig = {
+         setup_lspconf = "custom.lspconfig",
+      },
+   },
+}
+
+return M
---
+++ lua/custom/lspconfig.lua
@@ -0,0 +1,18 @@
+-- Hare Krishna
+local M = {}
+
+M.setup_lsp = function(attach, capabilities)
+   local lspconfig = require "lspconfig"
+
+   -- lspservers with default config
+   local servers = { "html", "cssls", "clangd", "emmet_ls", "angularls", "tsserver", "pyright" }
+
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = attach,
+         capabilities = capabilities,
+      }
+   end
+end
+
+return M
